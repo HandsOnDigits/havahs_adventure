01a837e36f3f5c5bad5a1763407878b052e05827
Sekaus <45533771+Sekaus@users.noreply.github.com>
2026-08-15 14:24:14 +0200

    Update NeedleCottonCropBlock.java

--- src/main/java/net/mcreator/havahsadventure/block/NeedleCottonCropBlock.java
+++ src/main/java/net/mcreator/havahsadventure/block/NeedleCottonCropBlock.java
@@ -3,6 +3,7 @@ package net.mcreator.havahsadventure.block;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.level.material.MapColor;
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
 import net.minecraft.world.level.block.state.StateDefinition;
@@ -15,20 +16,24 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.util.RandomSource;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.core.Direction;
 import net.minecraft.core.BlockPos;
 
 import net.mcreator.havahsadventure.procedures.NeedleCottonCropOnTickUpdateProcedure;
+import net.mcreator.havahsadventure.procedures.NeedleCottonCropOnBlockRightclickedProcedure;
 import net.mcreator.havahsadventure.procedures.NeedleCottonCropNeighbourBlockChangesProcedure;
 import net.mcreator.havahsadventure.init.HavahsAdventureModItems;
 
 public class NeedleCottonCropBlock extends Block {
-	public static final IntegerProperty GROTH = IntegerProperty.create("groth", 0, 3);
+	public static final IntegerProperty GROWTH = IntegerProperty.create("growth", 0, 3);
 
 	public NeedleCottonCropBlock() {
 		super(BlockBehaviour.Properties.of().mapColor(MapColor.SNOW).sound(SoundType.CROP).strength(0f, 10f).noCollission().randomTicks().isRedstoneConductor((bs, br, bp) -> false));
-		this.registerDefaultState(this.stateDefinition.any().setValue(GROTH, 0));
+		this.registerDefaultState(this.stateDefinition.any().setValue(GROWTH, 0));
 	}
 
 	@Override
@@ -49,12 +54,12 @@ public class NeedleCottonCropBlock extends Block {
 	@Override
 	protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
 		super.createBlockStateDefinition(builder);
-		builder.add(GROTH);
+		builder.add(GROWTH);
 	}
 
 	@Override
 	public BlockState getStateForPlacement(BlockPlaceContext context) {
-		return super.getStateForPlacement(context).setValue(GROTH, 0);
+		return super.getStateForPlacement(context).setValue(GROWTH, 0);
 	}
 
 	@Override
@@ -73,4 +78,21 @@ public class NeedleCottonCropBlock extends Block {
 		super.randomTick(blockstate, world, pos, random);
 		NeedleCottonCropOnTickUpdateProcedure.execute(world, pos.getX(), pos.getY(), pos.getZ(), blockstate);
 	}
+
+	@Override
+public InteractionResult useWithoutItem(BlockState blockstate, Level world, BlockPos pos, Player entity, BlockHitResult hit) {
+    super.useWithoutItem(blockstate, world, pos, entity, hit);
+    int x = pos.getX();
+    int y = pos.getY();
+    int z = pos.getZ();
+    double hitX = hit.getLocation().x;
+    double hitY = hit.getLocation().y;
+    double hitZ = hit.getLocation().z;
+    Direction direction = hit.getDirection();
+    
+    // Fix: Changed 'state' to 'blockstate'
+    NeedleCottonCropOnBlockRightclickedProcedure.execute(world, x, y, z, blockstate, entity);
+    
+    return InteractionResult.SUCCESS;
+}
 }
