33e9f774047070229fec5f2b5d412c62a5ccf10b
Sekaus <45533771+Sekaus@users.noreply.github.com>
2026-08-15 14:24:21 +0200

    Update NeedleCottonCropOnTickUpdateProcedure.java

--- src/main/java/net/mcreator/havahsadventure/procedures/NeedleCottonCropOnTickUpdateProcedure.java
+++ src/main/java/net/mcreator/havahsadventure/procedures/NeedleCottonCropOnTickUpdateProcedure.java
@@ -8,12 +8,12 @@ import net.minecraft.core.BlockPos;
 
 public class NeedleCottonCropOnTickUpdateProcedure {
 	public static void execute(LevelAccessor world, double x, double y, double z, BlockState blockstate) {
-		if ((getPropertyByName(blockstate, "groth") instanceof IntegerProperty _getip1 ? blockstate.getValue(_getip1) : -1) < 3) {
+		if ((getPropertyByName(blockstate, "growth") instanceof IntegerProperty _getip1 ? blockstate.getValue(_getip1) : -1) < 3) {
 			{
-				int _value = (getPropertyByName(blockstate, "groth") instanceof IntegerProperty _getip3 ? blockstate.getValue(_getip3) : -1) + 1;
+				int _value = (getPropertyByName(blockstate, "growth") instanceof IntegerProperty _getip3 ? blockstate.getValue(_getip3) : -1) + 1;
 				BlockPos _pos = BlockPos.containing(x, y, z);
 				BlockState _bs = world.getBlockState(_pos);
-				if (_bs.getBlock().getStateDefinition().getProperty("groth") instanceof IntegerProperty _integerProp && _integerProp.getPossibleValues().contains(_value))
+				if (_bs.getBlock().getStateDefinition().getProperty("growth") instanceof IntegerProperty _integerProp && _integerProp.getPossibleValues().contains(_value))
 					world.setBlock(_pos, _bs.setValue(_integerProp, _value), 3);
 			}
 		}
